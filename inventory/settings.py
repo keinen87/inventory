8d5de4f1437d128c99d7cc0343827fcce17a9a74
Nurbek Kuspanov <nurbek@inbox.ru>
2022-06-14 13:27:24 +0400

    updated settings

--- inventory/settings.py
+++ inventory/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from environs import Env
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -51,11 +56,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'inventory.urls'
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +123,17 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
+
+MEDIA_ROOT = os.getenv("MEDIA_ROOT", os.path.join(BASE_DIR, 'media'))
+
+STATIC_ROOT = os.getenv("STATIC_ROOT")
+
+MEDIA_URL = os.getenv('MEDIA_URL', '/media/')
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
